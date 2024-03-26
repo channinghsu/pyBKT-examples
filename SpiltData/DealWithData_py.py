e1bf04ff2067b5665faf943081b2bd350ca66b38
XCU8SZH <fixed-term.Channing.XU@cn.bosch.com>
2024-03-26 17:31:44 +0800

    Update data file paths and train size

--- SpiltData/DealWithData_py.py
+++ SpiltData/DealWithData_py.py
@@ -4,13 +4,13 @@ from sklearn.model_selection import train_test_split
 # 读取数据集文件 ct.csv
 # df = pd.read_csv('../data/as_delete_nan.csv')  # 根据实际情况选择正确的编码
 # df = pd.read_csv('../data/student-problem-middle.csv')
-df = pd.read_csv('../data/ct_relation.csv')
+df = pd.read_csv('./data/ct_relation.csv')
 # 按知识点分组，并获取每个知识点的总题数
 knowledge_point_counts = df['skill'].value_counts()
 
 
 # 定义一个函数，用于从每个知识点的题目中抽取一定比例的题目作为训练集
-def split_by_knowledge_point(df, knowledge_point, train_size=0.7, random_state=23):
+def split_by_knowledge_point(df, knowledge_point, train_size=0.8, random_state=23):
     knowledge_point_df = df[df['skill'] == knowledge_point]
     if len(knowledge_point_df) <= 1:
         print(f"Skipping knowledge point {knowledge_point} due to insufficient data.")
@@ -47,8 +47,8 @@ print("预测集总题数:", len(predict_df))
 
 # 将 train_df 保存为 TSV 文件
 # train_df.to_csv('../data/mooc_radar_train_set.tsv', sep='\t', index=False)
-train_df.to_csv('../data/ct_train.tsv', sep='\t', index=False)
+train_df.to_csv('./data/ct_train.tsv', sep='\t', index=False)
 
 # 将 predict_df 保存为 CSV 文件
 # predict_df.to_csv('../data/mooc_radar_predict_set.csv', index=False)
-predict_df.to_csv('../data/ct_predict.csv', index=False)
+predict_df.to_csv('./data/ct_predict.csv', index=False)
